5f02c72238f9710bb8fe0eb01af4433ebf8d7e6e
Vladimir Gorej <vladimir.gorej@gmail.com>
2022-10-28 21:50:08 +0200

    feat: add parsing mechanism

--- src/index.js
+++ src/index.js
@@ -2,7 +2,14 @@ import ApgExp from 'apg-js/src/apg-exp/apg-exp.js';
 
 import SABNF from './runtime-expression.bnf.js';
 
-const apgExp = new ApgExp(SABNF);
+export const test = (str) => {
+  const apgExp = new ApgExp(SABNF);
+  return apgExp.test(str);
+}
 
-export const test = apgExp.test.bind(apgExp);
+export const parse = (str) => {
+  const apgExp = new ApgExp(SABNF);
+  return apgExp.exec(str);
+}
+
 export { SABNF };
