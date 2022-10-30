876a700d358d78cb8d6fa17be7847d2f19672e59
Vladimir Gorej <vladimir.gorej@gmail.com>
2022-10-30 19:16:11 +0100

    test(grammar): test string conversion of grammar

--- test/runtime-expression.js
+++ test/runtime-expression.js
@@ -1,3 +1,5 @@
+import * as fs from 'node:fs';
+import * as path from 'node:path';
 import { assert } from 'chai';
 
 import { Grammar } from '../src/index.js'
@@ -6,4 +8,12 @@ describe('SABNF', function () {
   it('should export Grammar', function () {
     assert.isFunction(Grammar);
   });
+
+  it('should convert to string', function () {
+    const abnfFilePath = path.join('..', 'src', 'runtime-expression.bnf');
+    const abnf = fs.readFileSync(new URL(abnfFilePath, import.meta.url)).toString();
+    const grammar = new Grammar();
+
+    assert.strictEqual(abnf, grammar.toString());
+  })
 })
